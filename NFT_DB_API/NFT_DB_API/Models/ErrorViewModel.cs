e53763c942f54786209134e5057f00fe4b082dbc
SpiderAndCat <130514366+SpiderAndCat@users.noreply.github.com>
2024-01-10 17:14:05 -0700

    Created basic ASPdotNET MVC project
    No authentication type
    NOT configured for HTTPS
    .NET 6.0

---
+++ NFT_DB_API/NFT_DB_API/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NFT_DB_API.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
