d941bafa1339086a43ace79658aad64bda6219cd
SpiderAndCat <130514366+SpiderAndCat@users.noreply.github.com>
2024-01-10 18:13:34 -0700

    Successful API call & JSON map to Model `NFT`
    And is displayed through the Razor View NFTGallery

--- NFT_DB_API/NFT_DB_API/Controllers/NFTGalleryController.cs
+++ NFT_DB_API/NFT_DB_API/Controllers/NFTGalleryController.cs
@@ -1,12 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NFT_DB_API.Models;
+using System.IO.MemoryMappedFiles;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace NFT_DB_API.Controllers
 {
     public class NFTGalleryController : Controller
     {
-        public ViewResult Index()
+        public async Task<ViewResult> Index()
         {
+            // For this use case, the HttpClient is only used once
+            // To to clean resources, using{} is utilized, to clear the HttpClient when out of scope
+            using (var httpClient = new HttpClient())
+            {
+                const string API_URL = "https://eth-mainnet.g.alchemy.com/nft/v3/WwNubjj7sBMCmY53SIbT7Y3415W6MwbJ/getNFTMetadata?contractAddress=0xBd3531dA5CF5857e7CfAA92426877b022e612cf8&tokenId=0&refreshCache=false";
+                try
+                {
+                    var response = await httpClient.GetStringAsync(API_URL);
+                    NFT nfts = JsonSerializer.Deserialize<NFT>(response);
+                    return View(nfts);
+                }
+
+                catch (JsonException ex)
+                {
+                    return View(ex.Message);
+                }
+
+                catch (HttpRequestException ex)
+                {
+                    return View(ex.Message);
+                }
+            }
+
+
+
+
+
+
+            /*
             List<NFT> nfts = new List<NFT>
             {
                 new NFT {
@@ -30,6 +62,7 @@ namespace NFT_DB_API.Controllers
                 },
             };
             return View(nfts);
+            */
         }
     }
 }
--- NFT_DB_API/NFT_DB_API/Models/NFT.cs
+++ NFT_DB_API/NFT_DB_API/Models/NFT.cs
@@ -1,49 +1,36 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
 
 namespace NFT_DB_API.Models
 {
     public class NFT
     {
-        /* Expect this format:
-         
-        {
-            "nfts": [
-                {
-                    "tokenId": "<value>",
-                    "name": "<value>",
-                    "image": {
-                        "originalUrl": "<https IPFS URL>"
-                    }
-                }
-            ]
-        }
-
-         */
-
-        [Required]
-        [Display(Name = "ID")]
-        public int id { get; set; }
+        public Contract contract { get; set; }
 
         // The JSON passes a string, even though it's an INT
         // will assign int cast of TokenID to id
         [Required]
         [Display(Name = "Token ID")]
-        public string TokenID { get; set; }
+        public string tokenId { get; set; }
 
         [Required]
         [Display(Name = "NFT Name")]
-        public string Name { get; set; }
+        public string name { get; set; }
 
 
         // Strings can already be nullable by default
-        public ImageStructure Image { get; set; }
+        public ImageStructure image { get; set; }
 
 
+    }
+    public class Contract
+    {
+        public string address { get; set; }
     }
 
     public class ImageStructure
     {
         [Display(Name = "Image URL")]
-        public string URL { get; set; }
+        public string originalUrl { get; set; }
     }
 }
